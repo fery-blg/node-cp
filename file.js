dbebccad064995367299b9cc4f37de4ed495d232
fery-blg <ferielblg5@gmail.com>
2024-01-26 10:23:24 +0100

    node js cp

---
+++ file.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+
+
+
+const http = require('http')
+
+
+
+const node = http.createServer(function (req, res) {
+
+
+    console.log(req)
+
+        const html = '<h1> Hello Node</h1>\n'
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(html);
+        res.end();
+    
+
+
+    if (req.method === "POST" && req.url === "/") {
+        let body = ""
+        req.on("data", function (data) { body += data })
+        req.on("end", function () {
+            const object = JSON.parse(body)
+            const html = '<h1> message succesfully </h1>\n'
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(html);
+            res.end();
+        })
+    }})
+
+
+const PORT = 3000
+
+node.listen(PORT, function () {
+    console.log(`localhost listening on port ${PORT}`);
+}
+);
